9f2eb1ca9b2195661e63df405162350e7873d6fd
daniyaniazi <farihadania@hotmail.com>
2020-11-06 00:32:55 +0500

    Basics

---
+++ 1.basic/1.basic/1.basic.cs
@@ -0,0 +1,18 @@
+﻿using System; 
+
+namespace _1.basic //used for organization
+{
+    class Program // A class  - has memebers 
+    {
+        static void Main(string[] args)// Define a block - Main where our program starts   
+        {
+            Console.WriteLine("Hello World!"); // Comments
+            Console.Read();
+            System.Console.WriteLine("Fully Qulaified Name");
+
+        } // Define A block
+    }
+    
+}
+//General Structures
+// namespace -> classes -> members -> methods -> statements
