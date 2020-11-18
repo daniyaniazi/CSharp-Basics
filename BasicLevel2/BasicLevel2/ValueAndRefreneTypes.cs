39adf800c6f6021073b773cf22f55934edb2c3d4
daniyaniazi <farihadania@hotmail.com>
2020-11-18 17:22:50 +0500

    practice

--- BasicLevel2/BasicLevel2/ValueAndRefreneTypes.cs
+++ BasicLevel2/BasicLevel2/ValueAndRefreneTypes.cs
@@ -4,6 +4,17 @@
    // Value Type
     static void Main()
     {
+        int a = -5;
+        int b=a; //copy of a 
+
+        Console.WriteLine(a);
+        Console.WriteLine(b);
+
+        b=10 //change in b will not change a value
+
+        Console.WriteLine(a);
+        Console.WriteLine(b);
+
         int value = 10;
         Program obj = new Program();
         obj.functionTest(value);
@@ -26,6 +37,14 @@ class programRef
 {
     static void Main()
     {
+        //Arrays
+        int[] a = { -5 };
+        int[] b = a; // a and b both points to same refrence
+        Console.WriteLine(a[0]);
+        Console.WriteLine(b[0]);
+        b[0] = 10;
+        Console.WriteLine(a[0]);
+        Console.WriteLine(b[0]);
 
         Program obj = new Program();
         obj.value = 10;
@@ -36,7 +55,7 @@ class programRef
     }
     void funcTest(Program obj)
     {
-        obj.value = 200;
+        obj.value = 200; //refrence type can chnage inside a method
     }
 }
 
