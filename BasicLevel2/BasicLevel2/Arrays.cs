1a19f724750bb488e2c6ac848bfc53d89e083549
daniyaniazi <farihadania@hotmail.com>
2020-11-19 16:32:49 +0500

    More About Arrays

--- BasicLevel2/BasicLevel2/Arrays.cs
+++ BasicLevel2/BasicLevel2/Arrays.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace BasicLevel2
@@ -16,17 +17,34 @@ namespace BasicLevel2
             arr[0] = 55;
             arr[1] = 56;
 
-
             //single statement
-            int[] arr2 = new int[8];
+            //int[] arr2 = new int[8];
+            Console.WriteLine("Enter The size of an array ");
+            int size = Convert.ToInt32(Console.ReadLine());
+            int[] arr2 = new int[size];
             for (int i = 0; i < arr2.Length; i++)
             {
-                arr2[i] = i * i;
+                arr2[i] = Convert.ToInt32(Console.ReadLine());
             }
 
-            int[] arr3 = { 1, 2, 3, 4, 4, 5 };
-            int[] arr4 = new int[] { 22, 33, 44, 55, 660 };
+            int[] arr3 = { 1, 2, 4, 6, 5, 3 };
+            int[] arr4 = new int[] { 22, 33, 44, 55, 660 ,9};
             char[] charinp = { 'p', 'a', 'k' };
+
+            Console.WriteLine(Array.IndexOf(arr3, 3));
+            //Sort in Asc
+            Array.Sort(arr3);
+            // Reverse -> Desc
+            Array.Reverse(arr3);
+            //Comparing an array
+            if (arr3 == arr4)
+            {
+                Console.WriteLine("Equal"); 
+            }  //----------------OR-------------/
+            if (Enumerable.SequenceEqual(arr3,arr4))
+            {
+                Console.WriteLine("Equal");
+            }
 
             // 2 types of 2D array
             // Rectangular array
@@ -35,12 +53,18 @@ namespace BasicLevel2
             ///////////////RECTANGULAR ///////////////
             int[,] twoDarr; //declaration
             twoDarr = new int[2, 2];
-
+            
             twoDarr[0, 0] = 1;
             twoDarr[0, 1] = 2;
             twoDarr[1, 0] = 3;
             twoDarr[1, 1] = 4;
-            // dimension -> GetLength
+            //--------------OR-------------
+            int[,] grades = new int[3, 4] {
+                {1,2,3,4 } ,
+                {1,4,9,16 } ,
+                {1,16,81,256 }
+            };
+            // dimension -> GetLength(0) -> rows 
             for (int i = 0; i < twoDarr.GetLength(0); i++)
             {
                 for (int j = 0; j < twoDarr.GetLength(1); j++)
@@ -58,7 +82,15 @@ namespace BasicLevel2
             twoDarr[0, 1] = 12;
             twoDarr[1, 0] = 13;
             twoDarr[1, 1] = 14;
-
+            //--------OR------------------
+            int[][] gradesJagged = new int[3][] {
+               new int[] {1,2,3,4,5,6,7 } ,
+               new int[]{1,4,9,16,25 } ,
+               new int[]{1,16,81 }
+            };
+            //gradesJagged[].Length -> rows
+            //gradesJagged[0].Length -> column at index 0
+            Console.WriteLine(gradesJagged[0][3]);
             for (int i = 0; i < arrjag.Length; i++) // no of rows
             {
                 for (int j = 0; j < arrjag[i].Length; j++) // no of element in row 
