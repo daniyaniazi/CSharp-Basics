b8d42a873c5a66f3059df0edbde4280bf1084158
daniyaniazi <farihadania@hotmail.com>
2020-11-27 22:22:51 +0500

    Interfaces in c#

---
+++ Interfaces/Interfaces/ItestInterface.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interfaces
+{
+    interface ItestInterface
+    {
+        void add(int a , int b); //By default public and are abstract
+      
+    }
+    interface ItestInherit
+    {
+        void sub(int a,int b);
+    }
+    class ImplementationClass : ItestInherit
+    {
+        public void add(int a, int b) {
+            Console.WriteLine(a+b);
+        }
+        public void sub(int a, int b)
+        {
+            Console.WriteLine(a-b);
+        }
+        static void Main()
+        {
+            ImplementationClass obj = new ImplementationClass();
+            obj.add(29, 90);
+            obj.sub(34, 8);
+            Console.Read();
+            // We can also creafe refrence interfaces
+            ItestInherit i = obj; //obj is class object
+
+
+        }
+    }
+}
