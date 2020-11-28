85d067710b683a0e07b5382b69e5d4f4d538116e
daniyaniazi <farihadania@hotmail.com>
2020-11-28 21:17:13 +0500

    binding anonymous methods in c#

---
+++ Delegates/Delegates/AnonymousMethodscs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Delegates
+{
+    public delegate string greetDel(string name);
+    class AnonymousMethodscs
+    {
+
+        static void Main()
+        {
+            //Anonymous method
+            greetDel obj = delegate (string name)
+            {
+                return "Hello" + name;
+            };
+            Console.Write(obj("namae"));
+        }
+    }
+}
