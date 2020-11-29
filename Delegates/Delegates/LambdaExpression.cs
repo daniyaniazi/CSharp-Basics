aa96d5846cdc0adbf657d0f56f168784d7435fd8
daniyaniazi <farihadania@hotmail.com>
2020-11-30 03:03:35 +0500

    Create LambdaExpression.cs

---
+++ Delegates/Delegates/LambdaExpression.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DelegatesPart2
+   
+{
+    public delegate string greetDel(string name); 
+    //ShortHand of Anonymous Functions
+    class LambdaEcpression
+    { 
+   
+        static void Main()
+        {
+            //lambda Expressions
+            //Delegate already lnow th return type and parameters type
+            greetDel obj = (name) =>
+            {
+                return "hello " + name + "Good Day!";
+            };
+            Console.WriteLine(obj("Daniya"));
+            Console.Read();
+        }
+    }
+}
