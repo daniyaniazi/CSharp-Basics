446197505de569c79c360c5630793e5118ef8a4e
daniyaniazi <farihadania@hotmail.com>
2020-12-03 01:58:55 +0500

    Collection in c#
    ArrayList and hashTabels

---
+++ Collections/Collections/ArrayList.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+namespace ArrayListProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList al = new ArrayList();
+            Console.WriteLine(al.Capacity);
+
+            //Adding items
+            al.Add(200); al.Add(300);
+            Console.WriteLine(al.Capacity); // Capacity increased
+            ArrayList al2 = new ArrayList(10); //specifying capacity
+            //Insert in an arraylist
+            al.Insert(1, 350);
+            foreach (object obj in al)
+                Console.WriteLine(obj + "");
+
+            //removing an item'al
+            al.Remove(200);
+            // by index position
+            al.RemoveAt(1);
+            Console.Read();
+        }
+                    
+    }
+}
---
+++ Collections/Collections/HashTables.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+//collections are not type save 
+namespace Collections
+{
+    class HashTablesProgram
+    {
+        static void Main(string[] args)
+        {
+            Hashtable ht = new Hashtable();
+            ht.Add("name", "Josh");
+            ht.Add("roll no", 26);
+
+            foreach (object key in ht.Keys)
+                Console.WriteLine(key + " : " + ht[key]);
+            Console.Read();
+        }
+    }
+}
