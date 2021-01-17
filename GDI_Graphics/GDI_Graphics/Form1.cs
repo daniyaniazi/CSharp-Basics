7701af49b758f1d441ecd222b71776231a40e896
daniyaniazi <farihadania@hotmail.com>
2021-01-17 23:34:19 +0500

    draw rectangle, line and ellipse

---
+++ GDI_Graphics/GDI_Graphics/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GDI_Graphics
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.BackColor = Color.AliceBlue;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void Form1_Paint(object sender,PaintEventArgs e ) {
+            Graphics g = e.Graphics;
+            Pen p = new Pen(Color.Red, 4);
+            g.DrawLine(p, 0, 0, 250, 250);
+            g.DrawEllipse(p, 120, 120, 250, 250);
+            g.DrawRectangle(p, 30, 30, 200, 200);
+        }
+    }
+}
