d0c50a05c6e95f340bd659567fd705784b3cff14
daniyaniazi <farihadania@hotmail.com>
2021-01-17 21:31:59 +0500

    Linq intro

---
+++ LINQS/LINQS/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace LINQS
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            
+            int[] arr = { 12, 33, 44, 55, 66, 77, 88, 32, 34, 23, 54, 5, 676, 78, 87, 65, 9 };
+            var barr = from i in arr where i > 40 orderby i select i;
+
+            foreach (int x in barr)
+                Console.Write(x + " ");
+            Console.Read();
+        }
+    }
+}
