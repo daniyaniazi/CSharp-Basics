64470b99d1b61560921da429d369ba0d789cdd65
daniyaniazi <farihadania@hotmail.com>
2021-02-05 15:58:18 +0500

    Custom Exceptions

---
+++ CustomExceptions/CustomExceptions/CandidateClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomExceptions
+{
+    class CandidateClass
+    {
+            public int CandidateAge{ get; set; }
+            public string CandidateName { get; set; }
+       
+    }
+    class InvalidCandidate : Exception
+    {
+        public InvalidCandidate()
+        {
+
+        }
+        public InvalidCandidate(int age)
+            : base("Age limit is from 18 to 60 ! "+age )
+        {
+
+        }
+    }
+}
---
+++ CustomExceptions/CustomExceptions/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomExceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CandidateClass c1 = new CandidateClass();
+            try
+            {
+                Console.WriteLine("ENTER AGE : ");
+                c1.CandidateAge = Convert.ToInt32(Console.ReadLine());
+                ValidateCandidate(c1);
+
+                Console.WriteLine("ENTER NAME : ");
+                c1.CandidateName = Console.ReadLine();
+
+                Console.Write("CANDIDATE REGSTGER SUCCESFULYY");
+            }
+            catch(InvalidCandidate exc)
+            {
+                Console.WriteLine(exc.Message);
+            }
+            Console.Read();
+        }
+        private static void ValidateCandidate(CandidateClass cnd)
+        {
+            if (cnd.CandidateAge < 18 || cnd.CandidateAge > 60)
+                throw new InvalidCandidate(cnd.CandidateAge);
+            else
+                Console.WriteLine("AGE ACCEPTED");
+
+        }
+       
+    }
+}
