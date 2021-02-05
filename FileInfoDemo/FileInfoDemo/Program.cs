78e973d57f217184134927beb7d1cfbcc5cd034d
daniyaniazi <farihadania@hotmail.com>
2021-02-05 21:24:53 +0500

    added more concepts

---
+++ FileInfoDemo/FileInfoDemo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace FileInfoDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileInfo fi = new FileInfo(@"F:\fileinfoDemo.txt");
+            //FileStream fs = fi.Create();
+            //created
+
+            string inputtext = Console.ReadLine();
+            
+            //textmethod
+            StreamWriter strwr = fi.CreateText();
+            strwr.WriteLine(inputtext);
+            strwr.Close();
+
+
+            //Delete 
+            fi.Delete();
+
+        }
+    }
+}
--- GDI_Graphics/GDI_Graphics/Form1.cs
+++ GDI_Graphics/GDI_Graphics/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,10 +25,22 @@ namespace GDI_Graphics
 
         private void Form1_Paint(object sender,PaintEventArgs e ) {
             Graphics g = e.Graphics;
-            Pen p = new Pen(Color.Red, 4);
-            g.DrawLine(p, 0, 0, 250, 250);
-            g.DrawEllipse(p, 120, 120, 250, 250);
-            g.DrawRectangle(p, 30, 30, 200, 200);
+            Pen redpen =new Pen(Color.Red, 2);
+            g.DrawLine(redpen, 0, 0,250, 250);
+            g.DrawEllipse(redpen, 260, 120, 350, 150);
+            g.DrawRectangle(redpen, 30, 30, 200, 200);
+            Rectangle rt = new Rectangle(50, 50, 200, 100);
+            g.DrawArc(redpen, rt, 20, 140);
+
+
+            Rectangle rect = new Rectangle(150, 310, 100, 100);
+            Brush yellowBrush = new SolidBrush(Color.Beige);
+            g.FillRectangle(yellowBrush, rect);
+
+            Font fnt = new Font("Verdana", 16, FontStyle.Italic);
+            g.DrawString("M YSTRING", fnt, yellowBrush,new Point(0, 20));
+
+
         }
     }
 }
