822dacf2f39ee7e5b5d088f9af638d808741a0ed
daniyaniazi <farihadania@hotmail.com>
2021-02-08 09:09:34 +0500

    Create Program.cs

---
+++ BocingUnboxing/BocingUnboxing/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BocingUnboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 10;
+            //Implicit
+            Console.WriteLine("boxing");
+            Object obj = i;
+           
+            //Explicit
+            int num2 = (int)obj;
+            Console.WriteLine("Unboxing");
+
+        }
+    }
+}
